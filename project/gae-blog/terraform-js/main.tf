ccfedd66bc392cc5d449b3916c6e8f4ef2aacf65
Greg <gregeast94@gmail.com>
2024-11-18 16:10:24 -0500

    add 3 files

--- project/gae-blog/terraform-js/main.tf
+++ project/gae-blog/terraform-js/main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
   rule {
-    object_ownership = "BucketOwnerPreffered"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
@@ -41,22 +41,75 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 
 # bucket policy
 
-resource "aws_bucket_policy" "nextjs_bucket_policy" {
+resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
-  policy = jsondecode(({
-    version = "2012-10-17"
+  policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
-        Action    = "S3.GetObject"
+        Action    = "s3:GetObject"
         Resource  = "${aws_s3_bucket.nextjs_bucket.arn}/*"
 
       }
     ]
 
 
-  }))
+  })
+}
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio site"
+}
+
+# CloudFront Distrubtion
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id   = "S3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Next.js portfolio site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-nextjs-portfolio-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+
 }
--- project/gae-blog/terraform-js/state.tf
+++ project/gae-blog/terraform-js/state.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-9494"
+    bucket         = "ge-my-tf-website-state" #Created a new bucket
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-file"
+    dynamodb_table = "project-table" #Created a table
 
   }
 }
+
